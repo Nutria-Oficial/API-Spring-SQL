900363b4a195c5a3c5e7c965d804e55af44347d5
Giulia <giulia.gso.2008@gmail.com>
2025-10-01 07:15:56 -0300

    chore: subindo configurações iniciais e dependências

---
+++ ms-nutria-postgresql/src/main/java/org/example/msnutriapostgresql/MsNutriaPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.msnutriapostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNutriaPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNutriaPostgresqlApplication.class, args);
+	}
+
+}
---
+++ ms-nutria-postgresql/src/test/java/org/example/msnutriapostgresql/MsNutriaPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.msnutriapostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNutriaPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
