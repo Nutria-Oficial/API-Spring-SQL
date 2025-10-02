3b6e6bb0f1f86076ad16855efa0f67911c2520b5
Giulia <giulia.gso.2008@gmail.com>
2025-10-01 21:37:10 -0300

    refactor: implementa novas exceções e atualiza classe para adicionar os DTOs de resposta

--- ms-nutria-postgresql/src/main/java/org/example/msnutriapostgresql/exception/ManipuladorGlobalExcecoes.java
+++ ms-nutria-postgresql/src/main/java/org/example/msnutriapostgresql/exception/ManipuladorGlobalExcecoes.java
@@ -1,5 +1,7 @@
 package org.example.msnutriapostgresql.exception;
 
+import org.example.msnutriapostgresql.dto.exceptiondto.BadRequestDTO;
+import org.example.msnutriapostgresql.dto.exceptiondto.ErrorDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,18 +14,29 @@ import java.util.Map;
 @ControllerAdvice
 public class ManipuladorGlobalExcecoes{
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<BadRequestException> badRequestException(MethodArgumentNotValidException methodArgumentNotValidException){
+    public ResponseEntity<BadRequestDTO> badRequestException(MethodArgumentNotValidException methodArgumentNotValidException){
         Map<String, String> errors = new HashMap<>();
 
         methodArgumentNotValidException.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(),error.getDefaultMessage()));
-        BadRequestException badRequestException = new BadRequestException("Erro na validação da requisição:",errors);
+        BadRequestDTO errorsDTO = new BadRequestDTO(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.name(),"Erro na validação da requisição:", errors);
 
-        return new ResponseEntity<>(badRequestException,HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorsDTO,HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> notFoundException(NotFoundException notFoundException){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFoundException.getMessage());
+    public ResponseEntity<ErrorDTO> notFoundException(NotFoundException notFoundException){
+        ErrorDTO errorDTO = new ErrorDTO(HttpStatus.NOT_FOUND.value(),HttpStatus.NOT_FOUND.name(),notFoundException.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDTO);
+    }
+    @ExceptionHandler(DuplicateException.class)
+    public ResponseEntity<ErrorDTO> duplicateException(DuplicateException duplicateException){
+        ErrorDTO errorDTO = new ErrorDTO(HttpStatus.CONFLICT.value(),HttpStatus.CONFLICT.name(), duplicateException.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorDTO);
+    }
+    @ExceptionHandler(DatabaseInsertException.class)
+    public ResponseEntity<ErrorDTO> databaseInsertException(DatabaseInsertException databaseInsertException){
+        ErrorDTO errorDTO = new ErrorDTO(HttpStatus.UNPROCESSABLE_ENTITY.value(), HttpStatus.UNPROCESSABLE_ENTITY.name(), databaseInsertException.getMessage());
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errorDTO);
 
     }
 
