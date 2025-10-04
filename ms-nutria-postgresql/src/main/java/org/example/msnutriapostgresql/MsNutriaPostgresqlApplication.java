f029fe1d7d4a315399544391764a6e06f3a2e6fd
Lívia Izidório <liviaizidorio.li@gmail.com>
2025-10-04 17:44:19 -0300

    Revert "fix: desativa autoconfiguração reativa de segurança para Spring MVC"
    This reverts commit ed9d358a9e48e0613276756dfcd8d23bdea877bd.

--- ms-nutria-postgresql/src/main/java/org/example/msnutriapostgresql/MsNutriaPostgresqlApplication.java
+++ ms-nutria-postgresql/src/main/java/org/example/msnutriapostgresql/MsNutriaPostgresqlApplication.java
@@ -2,9 +2,8 @@ package org.example.msnutriapostgresql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {ReactiveUserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 public class MsNutriaPostgresqlApplication {
 
   public static void main(String[] args) {
