ed545fe23e3d55f306684e4a3a446ac545ab953b
Lívia Izidório <liviaizidorio.li@gmail.com>
2025-10-04 19:02:06 -0300

    fix: excluindo auto-configurações reativas do Spring Security

--- ms-nutria-postgresql/src/main/java/org/example/msnutriapostgresql/MsNutriaPostgresqlApplication.java
+++ ms-nutria-postgresql/src/main/java/org/example/msnutriapostgresql/MsNutriaPostgresqlApplication.java
@@ -2,8 +2,13 @@ package org.example.msnutriapostgresql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        ReactiveSecurityAutoConfiguration.class,
+        ReactiveUserDetailsServiceAutoConfiguration.class
+})
 public class MsNutriaPostgresqlApplication {
 
   public static void main(String[] args) {
