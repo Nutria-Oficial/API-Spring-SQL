d58bea2f14f67256e05f67401a2a89cb2dadf5a8
Lívia Izidório <liviaizidorio.li@gmail.com>
2025-10-20 08:53:19 -0300

    feat: adicionando cors apenas para o admin, para conseguir usar no navegador

--- ms-nutria-postgresql/src/main/java/org/example/msnutriapostgresql/security/SecurityConfig.java
+++ ms-nutria-postgresql/src/main/java/org/example/msnutriapostgresql/security/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,33 +28,56 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(
-            authorize ->
-                authorize
-                    .requestMatchers("/admin/**")
-                    .hasRole("NUTRIA_ADMIN")
-                    .requestMatchers("/usuarios/**")
-                    .hasAnyRole("NUTRIA_ADMIN", "NUTRIA")
-                    .anyRequest()
-                    .authenticated())
-        .httpBasic(Customizer.withDefaults())
-        .exceptionHandling(exception -> exception.accessDeniedHandler(customAccessDeniedHandler));
+    http
+            // 🔹 Ativa CORS e vincula a configuração personalizada
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize ->
+                    authorize
+                            .requestMatchers("/admin/**").hasRole("NUTRIA_ADMIN")
+                            .requestMatchers("/usuarios/**").hasAnyRole("NUTRIA_ADMIN", "NUTRIA")
+                            .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults())
+            .exceptionHandling(exception -> exception.accessDeniedHandler(customAccessDeniedHandler));
+
     return http.build();
+  }
+
+  //cors apenas para admin para usar no navegador
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+
+    config.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://area-restrita-krae.onrender.com/"
+    ));
+
+    config.setAllowedMethods(List.of("GET"));
+    config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+    config.setExposedHeaders(List.of("Authorization"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration("/admin/**", config);
+
+    return source;
   }
 
   @Bean
   public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder) {
     UserDetails admin =
-        User.withUsername("nutriaAdmin")
-            .password(passwordEncoder.encode("nutriaAdmin123"))
-            .roles("NUTRIA_ADMIN")
-            .build();
+            User.withUsername("nutriaAdmin")
+                    .password(passwordEncoder.encode("nutriaAdmin123"))
+                    .roles("NUTRIA_ADMIN")
+                    .build();
     UserDetails user =
-        User.withUsername("nutria")
-            .password(passwordEncoder.encode("nutria123"))
-            .roles("NUTRIA")
-            .build();
+            User.withUsername("nutria")
+                    .password(passwordEncoder.encode("nutria123"))
+                    .roles("NUTRIA")
+                    .build();
     return new InMemoryUserDetailsManager(admin, user);
   }
 
